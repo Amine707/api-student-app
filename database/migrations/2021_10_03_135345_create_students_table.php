259dd27ef1895959ea0e84267fdee650ddc25f62
Amine707 <ia_bacha@esi.dz>
2021-10-03 15:18:26 +0100

    data migration,controllers and routes added

--- database/migrations/2021_10_03_135345_create_students_table.php
+++ database/migrations/2021_10_03_135345_create_students_table.php
@@ -14,7 +14,9 @@ class CreateStudentsTable extends Migration
     public function up()
     {
         Schema::create('students', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
+            $table->string('name');
+            $table->string('course');
             $table->timestamps();
         });
     }
